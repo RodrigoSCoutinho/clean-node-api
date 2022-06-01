3ba45fc74016a460772c41c9ef0db6a6964c3708
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2022-06-01 07:39:32 -0300

    add to news RG

--- src/domain/entities/challenge.ts
+++ src/domain/entities/challenge.ts
@@ -9,7 +9,7 @@ export class Challenge extends Entity<ChallengeProps>{
         super(props, id);
      }
 
-     static create(props: ChallengeProps, id?: string){
+    static create(props: ChallengeProps, id?: string){
          const challenge = new Challenge(props, id);
 
          return challenge;
---
+++ src/tests/repositories/in-memory-challenges-repository.ts
@@ -0,0 +1,18 @@
+import { StudentsRepository } from "../../application/repositories/StudentsRepository";
+import { Student } from "../../domain/entities/student";
+
+
+class InMemoryRepository implements StudentsRepository {
+    public items: Student[] = [];
+  
+  
+    async findById(id: string): Promise<Student | null> {
+      const student =  this.items.find(student => student.id === id)
+
+      if(!student) {
+          return null;
+      }
+
+          return student;
+   }
+}
